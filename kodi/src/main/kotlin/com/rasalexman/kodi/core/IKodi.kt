0788906d82bd658f7da1ca027b1d35747c893746
Alexander <sphc@yandex.ru>
2021-04-04 06:07:55 +0300

    added removeListener for IKodi.kt

--- kodi/src/main/kotlin/com/rasalexman/kodi/core/IKodi.kt
+++ kodi/src/main/kotlin/com/rasalexman/kodi/core/IKodi.kt
@@ -249,6 +249,23 @@ inline fun <reified T : Any> IKodi.addBindingListener(
 }
 
 /**
+ * Remove listener for event when instance is already binded to dependency graph
+ *
+ * @param tag - String instance tag (Optional)
+ * @param scope - String of instance scope (Optional)
+ * @param listener - event handling function (Optional),
+ * if there is a null, it remove all binding listeners for selected tad and scope
+ *
+ */
+inline fun <reified T : Any> IKodi.removeBindingListener(
+        tag: String? = null,
+        scope: String? = null,
+        noinline listener: InstanceHandler<T>?
+) {
+    removeListener(tag, scope, listener, BindingEvent.BIND)
+}
+
+/**
  * Add listener for event when instance is already unbinded(removed) from dependency graph
  *
  * @param tag - String instance tag (Optional)
@@ -262,6 +279,23 @@ inline fun <reified T : Any> IKodi.addUnbindingListener(
         noinline listener: InstanceHandler<T>
 ) {
     addListener(tag, scope, listener, BindingEvent.UNBIND)
+}
+
+/**
+ * Remove listener for event when instance is already removed to dependency graph
+ *
+ * @param tag - String instance tag (Optional)
+ * @param scope - String of instance scope (Optional)
+ * @param listener - event handling function (Optional),
+ * if there is a null, it remove all unbinding listeners for selected tad and scope
+ *
+ */
+inline fun <reified T : Any> IKodi.removeUnbindingListener(
+        tag: String? = null,
+        scope: String? = null,
+        noinline listener: InstanceHandler<T>?
+) {
+    removeListener(tag, scope, listener, BindingEvent.UNBIND)
 }
 
 /**
@@ -282,6 +316,30 @@ inline fun <reified T : Any> IKodi.addListener(
     val tagToWrap = tag.or { genericName<T>() }.asTag()
     val scopeToWrap = scope?.asScope()
     Kodi.addListener(tagToWrap, scopeToWrap.or { defaultScope }, listener, event)
+}
+
+/**
+ * Remove listener for bind or unbind event for instance
+ *
+ * @param tag - String instance tag (Optional)
+ * @param scope - String of instance scope (Optional)
+ * @param listener - event handling function (Optional), that was listen the event
+ * @param event - [BindingEvent]
+ *
+ */
+inline fun <reified T : Any> IKodi.removeListener(
+        tag: String? = null,
+        scope: String? = null,
+        noinline listener: InstanceHandler<T>?,
+        event: BindingEvent
+) {
+    val tagToWrap = tag.or { genericName<T>() }.asTag()
+    val scopeToWrap = scope?.asScope()
+    listener?.let {
+        Kodi.removeListener(tagToWrap, scopeToWrap.or { defaultScope }, listener, event)
+    }.or {
+        Kodi.removeAllListeners(tagToWrap, scopeToWrap.or { defaultScope })
+    }
 }
 
 /**
