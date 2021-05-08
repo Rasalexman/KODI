76d41fc3228cdfb4658237d1ff45aaa1bd06a453
Alexander <sphc@yandex.ru>
2021-05-08 16:23:33 +0300

    one more test version

--- buildSrc/src/main/kotlin/appdependencies/Builds.kt
+++ buildSrc/src/main/kotlin/appdependencies/Builds.kt
@@ -8,22 +8,22 @@ object Builds {
     const val APP_ID = "com.mincor.kodiexample"
 
     object App {
-        const val VERSION_CODE = 10009
-        const val VERSION_NAME = "10009"
+        const val VERSION_CODE = 10010
+        const val VERSION_NAME = "10010"
     }
 
     object Kodi {
-        const val VERSION_NAME = "1.5.11"
+        const val VERSION_NAME = "1.5.12"
     }
 
     object KodiAndroidX {
-        const val VERSION_CODE = 105011
-        const val VERSION_NAME = "1.5.11"
+        const val VERSION_CODE = 105012
+        const val VERSION_NAME = "1.5.12"
     }
 
     object KodiReflect {
-        const val VERSION_CODE = 10128
-        const val VERSION_NAME = "1.1.28"
+        const val VERSION_CODE = 10129
+        const val VERSION_NAME = "1.1.29"
     }
 }
 
